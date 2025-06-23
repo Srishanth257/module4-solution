9a284f76cfef16205a0e7fb3fb2d43ff39e69b73
Srishanth257 <work.srishanth@gmail.com>
2025-06-23 15:56:23 +0530

    Create helloSpeaker.js

---
+++ helloSpeaker.js
@@ -0,0 +1,11 @@
+// Wrap in an IIFE to avoid polluting global scope
+(function(window) {
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+
+  helloSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
